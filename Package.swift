ecd3eb9286c5d36ea02f9d41771d976d6b402331
Łukasz Dziedzic <dziedzic@alfabety.pl>
2022-10-08 21:59:24 +0200

    a tak?

--- Package.swift
+++ Package.swift
@@ -37,7 +37,7 @@ let package = Package(
         
             .target(
                     name: "UnicodeData",
-                    //path: "UnicodeData",
+                    dependencies: ["UnicodeNames"],
                     resources: [
                         .process("UnicodeData/flatUnicode.txt"),
                         .process("UnicodeData/glyphNamesToUnicodeAndCategories.txt")]
