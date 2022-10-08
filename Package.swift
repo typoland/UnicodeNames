1d102ffe97e2c8171303550a67938c99a4cc3862
Łukasz Dziedzic <dziedzic@alfabety.pl>
2022-10-09 01:23:47 +0200

    A teraz?

--- Package.swift
+++ Package.swift
@@ -24,24 +24,18 @@ let package = Package(
         )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "UnicodeNames",
             dependencies: [
                 .product(name: "Collections", package: "swift-collections")
             ],
             resources: [
-                .copy("UnicodeData")]
+                .copy("UnicodeData")
+            ]
+            
         ),
         .testTarget(
             name: "UnicodeNamesTests",
             dependencies: ["UnicodeNames"]),
-//        .target(
-//            name: "UnicodeNames",
-//            //dependencies: ["UnicodeNames"],
-//
-//        ),
-        
     ]
 )
